1feb7f02c99b04010a1e94dc6a3528740eed7666
AydinTokuslu <aydintokuslu78@gmail.com>
2024-01-10 00:03:44 +0100

    updated

--- aws/Project-503-Capstone-Project-Blog-Page-App-Django-on-AWS-Environment/userdata.sh
+++ aws/Project-503-Capstone-Project-Blog-Page-App-Django-on-AWS-Environment/userdata.sh
@@ -4,7 +4,7 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
-git clone https://$TOKEN@https://github.com/AydinTokuslu/my-aws-projects.git
+git clone https://$TOKEN@github.com/AydinTokuslu/my-aws-projects.git
 cd /home/ubuntu/my-aws-capstone-project/aws/Project-503-Capstone-Project-Blog-Page-App-Django-on-AWS-Environment
 apt install python3-pip -y
 apt-get install python3.7-dev default-libmysqlclient-dev -y
